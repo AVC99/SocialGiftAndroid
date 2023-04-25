d06fd75ab5386757a1b678bd1eb9d549e5a79620
AVC99 <arnauvivescomes@gmail.com>
2023-04-25 02:17:46 +0200

    Added basic logic to the register and login

--- app/src/main/java/com/example/socialgift/ui/views/login/LoginActivity.java
+++ app/src/main/java/com/example/socialgift/ui/views/login/LoginActivity.java
@@ -2,15 +2,53 @@ package com.example.socialgift.ui.views.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.socialgift.R;
+import com.example.socialgift.ui.views.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button forgotPasswordButton;
+    private Button registerButton;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.login_login_button);
+        forgotPasswordButton = findViewById(R.id.login_forgot_password);
+        registerButton = findViewById(R.id.login_create_account_button);
+        emailEditText = findViewById(R.id.login_email);
+        passwordEditText = findViewById(R.id.login_password);
+
+        loginButton.setOnClickListener(v -> {
+            if(emailEditText.getText().toString().isEmpty() || passwordEditText.getText().toString().isEmpty()){
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+            }else {
+                loginToSocialGift();
+            }
+        });
+
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+
+
+    }
+
+    private void loginToSocialGift() {
+        //TODO: Call API and login to social gift
     }
 }
--- app/src/main/java/com/example/socialgift/ui/views/register/RegisterActivity.java
+++ app/src/main/java/com/example/socialgift/ui/views/register/RegisterActivity.java
@@ -2,15 +2,51 @@ package com.example.socialgift.ui.views.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.socialgift.R;
+import com.example.socialgift.ui.views.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private EditText surnameEditText;
+    private Button createAccountButton;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nameEditText = findViewById(R.id.register_name_edit_text);
+        surnameEditText = findViewById(R.id.register_surname_edit_text);
+        emailEditText = findViewById(R.id.register_email_edit_text);
+        passwordEditText = findViewById(R.id.register_password_edit_text);
+        createAccountButton = findViewById(R.id.register_create_account_button);
+        loginButton = findViewById(R.id.register_login_button);
+
+        createAccountButton.setOnClickListener(v -> {
+            if(nameEditText.getText().toString().isEmpty() || surnameEditText.getText().toString().isEmpty()
+                    || emailEditText.getText().toString().isEmpty() || passwordEditText.getText().toString().isEmpty()){
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+            }else {
+                createAccount();
+            }
+        });
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void createAccount() {
     }
 }
