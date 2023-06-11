0bf82685645665252ef556c9435ca3432cc1dbf0
AVC99 <arnauvivescomes@gmail.com>
2023-06-11 23:55:43 +0200

    Removed Warnings

--- app/src/main/java/com/example/socialgift/ui/views/create/CreateWishlistFragment.java
+++ app/src/main/java/com/example/socialgift/ui/views/create/CreateWishlistFragment.java
@@ -23,20 +23,18 @@ import com.example.socialgift.R;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Objects;
 
 
 public class CreateWishlistFragment extends Fragment {
-    private Button createWishlistButton;
     private EditText wishlistNameEditText;
     private EditText wishlistDescriptionEditText;
     private EditText wishlistDateEditText;
 
 
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy", Locale.ENGLISH);
-    private SimpleDateFormat sendDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy", Locale.ENGLISH);
+    private final SimpleDateFormat sendDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
     private Date date;
-    private Calendar myCalendar = Calendar.getInstance();
+    private final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,13 +44,13 @@ public class CreateWishlistFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        this.createWishlistButton = view.findViewById(R.id.create_wishlist_create_button);
+        Button createWishlistButton = view.findViewById(R.id.create_wishlist_create_button);
         this.wishlistDateEditText = view.findViewById(R.id.create_wishlist_date_edit_text);
         this.wishlistDescriptionEditText = view.findViewById(R.id.create_wishlist_description_edit_text);
         this.wishlistNameEditText = view.findViewById(R.id.create_wishlist_name_edit_text);
         setUpDateEditText();
 
-        this.createWishlistButton.setOnClickListener(v -> {
+        createWishlistButton.setOnClickListener(v -> {
             if (wishlistDateEditText.getText().toString().isEmpty() || wishlistDescriptionEditText.getText().toString().isEmpty() || wishlistNameEditText.getText().toString().isEmpty()) {
                 Toast.makeText(getContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
             } else {
@@ -87,11 +85,9 @@ public class CreateWishlistFragment extends Fragment {
             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
             updateLabel();
         };
-        wishlistDateEditText.setOnClickListener(v -> {
-            new DatePickerDialog(getContext(), date, myCalendar
-                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-        });
+        wishlistDateEditText.setOnClickListener(v -> new DatePickerDialog(getContext(), date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show());
 
     }
 
